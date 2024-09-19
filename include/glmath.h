2435991f57d27887c7e8836c60e35b56406d3a01
seonjo1 <tjdgus9684@gmail.com>
2024-09-19 19:01:21 +0900

    Added glmath.h : glmath 헤더 생성 vec2 class 생성

---
+++ include/glmath.h
@@ -0,0 +1,23 @@
+#ifndef GLMATH_H
+# define GLMATH_H
+
+namespace glmath{
+
+class vec2 {
+private:
+
+public:
+    float x;
+    float y;
+
+	vec2(float x, float y);
+	vec2(const vec2& copy);
+	vec2& operator=(const vec2& copy);
+	vec2 operator+(const vec2& copy) const;
+	vec2 operator-(const vec2& copy) const;
+	vec2 operator*(const vec2& copy) const;
+};
+
+}
+
+#endif
