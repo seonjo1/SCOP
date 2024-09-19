7628a65a995447b29ec7112e06dc797efbed3023
seonjo1 <tjdgus9684@gmail.com>
2024-09-19 19:01:43 +0900

    Added glmath.cpp: glmath.cpp 파일 생성 class vec2 함수 구현

---
+++ src/glmath.cpp
@@ -0,0 +1,27 @@
+#include "../include/glmath.h"
+
+// class vec2
+
+glmath::vec2::vec2(float x, float y)
+	: x(x), y(y) {};
+
+glmath::vec2::vec2(const vec2& copy)
+	: x(copy.x), y(copy.y) {};
+
+glmath::vec2& glmath::vec2::operator=(const glmath::vec2& copy) {
+	this->x = copy.x;
+	this->y = copy.y;
+	return *this;
+}
+
+glmath::vec2 glmath::vec2::operator+(const vec2& copy) const {
+	return glmath::vec2(this->x + copy.x, this->y + copy.y);
+}
+
+glmath::vec2 glmath::vec2::operator-(const vec2& copy) const{
+	return glmath::vec2(this->x - copy.x, this->y - copy.y);
+}
+
+glmath::vec2 glmath::vec2::operator*(const vec2& copy) const{
+	return glmath::vec2(this->x * copy.x, this->y * copy.y);
+}
