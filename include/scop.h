bd9f146b311f8fa744352fa70173943d89907b6c
seonjo1 <tjdgus9684@gmail.com>
2024-09-19 13:10:14 +0900

    Added scop.h: main.cpp를 위한 헤더 추가

---
+++ include/scop.h
@@ -0,0 +1,11 @@
+#ifndef SCOP_H
+# define SCOP_H
+
+# include <memory>
+# include <string>
+# include <optional>
+# include <spdlog/spdlog.h>
+# include <glad/glad.h>
+# include <GLFW/glfw3.h>
+
+#endif
