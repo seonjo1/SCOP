278575051ccd010997fd697cf1a50ac92fcd09aa
seonjo1 <tjdgus9684@gmail.com>
2024-09-19 19:16:10 +0900

    Update glmath.cpp: class vec3 함수 추가

--- src/glmath.cpp
+++ src/glmath.cpp
@@ -28,3 +28,34 @@ glmath::vec2 glmath::vec2::operator-(const vec2& copy) const{
 glmath::vec2 glmath::vec2::operator*(const vec2& copy) const{
 	return glmath::vec2(this->x * copy.x, this->y * copy.y);
 }
+
+
+// class vec3
+
+glmath::vec3::vec3(float x)
+	: x(x), y(x), z(x) {};
+
+glmath::vec3::vec3(float x, float y, float z)
+	: x(x), y(y), z(z) {};
+
+glmath::vec3::vec3(const vec3& copy)
+	: x(copy.x), y(copy.y), z(copy.z) {};
+
+glmath::vec3& glmath::vec3::operator=(const glmath::vec3& copy) {
+	this->x = copy.x;
+	this->y = copy.y;
+	this->z = copy.z;
+	return *this;
+}
+
+glmath::vec3 glmath::vec3::operator+(const vec3& copy) const {
+	return glmath::vec3(this->x + copy.x, this->y + copy.y, this->z + copy.z);
+}
+
+glmath::vec3 glmath::vec3::operator-(const vec3& copy) const{
+	return glmath::vec3(this->x - copy.x, this->y - copy.y, this->z - copy.z);
+}
+
+glmath::vec3 glmath::vec3::operator*(const vec3& copy) const{
+	return glmath::vec3(this->x * copy.x, this->y * copy.y, this->z * copy.z);
+}
