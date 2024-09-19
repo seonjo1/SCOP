fc903646cd978bad0246df667c932c0197e86dbc
seonjo1 <tjdgus9684@gmail.com>
2024-09-19 19:23:11 +0900

    Update glmath.cpp: class vec4 추가

--- src/glmath.cpp
+++ src/glmath.cpp
@@ -59,3 +59,35 @@ glmath::vec3 glmath::vec3::operator-(const vec3& copy) const{
 glmath::vec3 glmath::vec3::operator*(const vec3& copy) const{
 	return glmath::vec3(this->x * copy.x, this->y * copy.y, this->z * copy.z);
 }
+
+
+// class vec4
+
+glmath::vec4::vec4(float x)
+	: x(x), y(x), z(x), w(x) {};
+
+glmath::vec4::vec4(float x, float y, float z, float w)
+	: x(x), y(y), z(z), w(w) {};
+
+glmath::vec4::vec4(const vec4& copy)
+	: x(copy.x), y(copy.y), z(copy.z), w(copy.w) {};
+
+glmath::vec4& glmath::vec4::operator=(const glmath::vec4& copy) {
+	this->x = copy.x;
+	this->y = copy.y;
+	this->z = copy.z;
+	this->w = copy.w;
+	return *this;
+}
+
+glmath::vec4 glmath::vec4::operator+(const vec4& copy) const {
+	return glmath::vec4(this->x + copy.x, this->y + copy.y, this->z + copy.z, this->w + copy.w);
+}
+
+glmath::vec4 glmath::vec4::operator-(const vec4& copy) const{
+	return glmath::vec4(this->x - copy.x, this->y - copy.y, this->z - copy.z, this->w - copy.w);
+}
+
+glmath::vec4 glmath::vec4::operator*(const vec4& copy) const{
+	return glmath::vec4(this->x * copy.x, this->y * copy.y, this->z * copy.z, this->w * copy.w);
+}
