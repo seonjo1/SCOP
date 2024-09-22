ab0fa55eda7e86d9ae3d9a0849dd87af6f5e447a
seonjo1 <tjdgus9684@gmail.com>
2024-09-22 23:24:20 +0900

    Update scop.h: glmath.h, glload.h, iostream 헤더 추가

--- include/scop.h
+++ include/scop.h
@@ -4,8 +4,10 @@
 # include <memory>
 # include <string>
 # include <optional>
-# include <spdlog/spdlog.h>
 # include <glad/glad.h>
 # include <GLFW/glfw3.h>
+# include <iostream>
+# include "glmath.h"
+# include "glload.h"
 
 #endif
