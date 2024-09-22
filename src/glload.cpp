3a40c012d9a9372755db81cb5660db1cdd92195f
seonjo1 <tjdgus9684@gmail.com>
2024-09-22 23:19:26 +0900

    Update glload.cpp: loadShaderFile(), loadBmpImag(), isBmpFile() 추가

--- src/glload.cpp
+++ src/glload.cpp
@@ -1,32 +1,100 @@
 #include "../include/glload.h"
 
-std::unique_ptr<uint8_t[]> glload::loadBmpImg(char* const fileName, int* width, int* height, int* channelCount) {
-	std::ifstream fin;
-	fin.open(fileName, std::ios::binary);
-	if (!fin.is_open()) throw std::runtime_error("Could not open BMP file.");
+bool glload::isBmpFile(std::string fileName) {
 
-	glload::BMPFileHeader fileHeader;
-    fin.read(reinterpret_cast<char*>(&fileHeader), sizeof(fileHeader));
+	int length = fileName.size();
+	
+	if (length < 4) {
+		return false;
+	}
+	
+	std::string ext = fileName.substr(length - 4);
+	std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+	
+	return ext == ".bmp";
+}
 
-	// 유효한 BMP 파일인지 확인
-    if (fileHeader.bfType != 0x4D42) {
-        throw std::runtime_error("Not a valid BMP file.");
+std::unique_ptr<uint8_t[]> glload::loadBmpImg(const char* fileName, int* width, int* height, int* channelCount) {
+
+	if (!isBmpFile(fileName)) {
+		std::cerr << "this file is not bmp format" << std::endl;
+		return nullptr;
+	}
+
+	std::ifstream fin(fileName, std::ios::binary);
+	if (!fin.is_open()) {
+		std::cerr << fileName << " is not exist" << std::endl;
+		return nullptr;
+	}
+	
+	char fileHeader[54];
+    fin.read(fileHeader, 54);
+
+    if (*reinterpret_cast<uint16_t*>(&fileHeader[0]) != 0x4D42) {
+		std::cerr << fileName << " is invalid bmp file" << std::endl;
+        return nullptr;
     }
 
-	glload::BMPInfoHeader bmpInfo;
-    fin.read(reinterpret_cast<char*>(&bmpInfo), sizeof(bmpInfo));
+	uint32_t imgOffBits = *reinterpret_cast<uint32_t*>(&fileHeader[10]);
+	uint32_t imgSize = *reinterpret_cast<uint32_t*>(&fileHeader[34]);
+	int32_t imgWidth = *reinterpret_cast<int32_t*>(&fileHeader[18]);
+	int32_t imgHeight = *reinterpret_cast<int32_t*>(&fileHeader[22]);
+	uint16_t imgBitCount = *reinterpret_cast<uint16_t*>(&fileHeader[28]);
+	uint32_t imgCompression = *reinterpret_cast<uint32_t*>(&fileHeader[30]);
 
-    // 데이터 할당
-    std::unique_ptr<uint8_t[]> data = std::make_unique<uint8_t[]>(bmpInfo.biSizeImage);
+	if (imgCompression) {
+		std::cerr << "this bmp file is compressed" << std::endl;
+		return nullptr;
+	}
 
-	// 데이터 옮기기
-    fin.seekg(fileHeader.bfOffBits, std::ios::beg);
-    fin.read(reinterpret_cast<char*>(data.get()), bmpInfo.biSizeImage);
-	*width = bmpInfo.biWidth;
-	*height = bmpInfo.biHeight;
-	*channelCount =  bmpInfo.biBitCount / 8;
-    
+	if (!imgSize) {
+		imgSize = std::abs(imgWidth * imgHeight * (imgBitCount / 8));
+	}
+
+    std::unique_ptr<uint8_t[]> data = std::make_unique<uint8_t[]>(imgSize);
+
+    fin.seekg(imgOffBits, std::ios::beg);
+    fin.read(reinterpret_cast<char*>(data.get()), imgSize);
+	
+	if (fin.fail()) {
+		std::cerr << "Error: Could not read the BMP image data." << std::endl;
+		return nullptr;
+	}
+	
+	if (imgHeight < 0) {
+		imgHeight = -imgHeight;
+		int rowSize = imgWidth * (imgBitCount / 8);
+		std::vector<uint8_t> tmp(rowSize);
+		for (int i = 0; i < imgHeight / 2; i++) {
+			uint8_t* upperRow = &data[i * rowSize];
+			uint8_t* lowerRow = &data[((imgHeight - 1) - i) * rowSize];
+			std::copy(upperRow, upperRow + rowSize, tmp.begin());
+			std::copy(lowerRow, lowerRow + rowSize, upperRow);
+			std::copy(tmp.begin(), tmp.end(), lowerRow);
+		}
+	}
+
+	*width = imgWidth;
+	*height = imgHeight;
+	*channelCount =  imgBitCount / 8;
+
 	fin.close();
 
     return data;
+}
+
+std::optional<std::string> glload::loadShaderFile(const std::string& fileName) {
+
+	std::ifstream fin(fileName);
+	
+	if (!fin.is_open()) {
+		std::cout << "failed to open shader file: " << fileName << std::endl;
+		return {};
+	}
+	
+	std::stringstream code;
+	code << fin.rdbuf();
+	fin.close();
+
+	return code.str();
 }
