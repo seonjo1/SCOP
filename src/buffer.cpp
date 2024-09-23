4843f064d8b0a96b3e33b8846c1e9eccb6d9fc19
seonjo1 <tjdgus9684@gmail.com>
2024-09-23 11:17:09 +0900

    Update buffer.cpp: buffer.cpp 구현

---
+++ src/buffer.cpp
@@ -0,0 +1,22 @@
+#include "../include/buffer.h"
+
+std::shared_ptr<Buffer> Buffer::create(GLenum type, GLsizeiptr size,
+										const void* data, GLenum usage) {
+	std::shared_ptr<Buffer> buffer(new Buffer());
+	buffer->genBuffer(type, size, data, usage);
+
+	return buffer;
+}
+
+Buffer::~Buffer() {
+	if (!m_buffer) {
+		glDeleteBuffers(1, &m_buffer);
+	}
+}
+
+void Buffer::genBuffer(GLenum type, GLsizeiptr size,
+						const void* data, GLenum usage) {
+	glGenBuffers(1, &m_buffer);
+	glBindBuffer(type, m_buffer);
+	glBufferData(type, size, data, usage);
+}
