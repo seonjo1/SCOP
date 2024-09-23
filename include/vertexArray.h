2e41e6440e3bf466eea30b78e7d82bd747bc921b
seonjo1 <tjdgus9684@gmail.com>
2024-09-23 10:28:25 +0900

    Update vertexArray.h: vertexArray.h 구현

--- include/vertexArray.h
+++ include/vertexArray.h
@@ -1,4 +1,20 @@
 #ifndef VERTEXARRAY_H
 # define VERTEXARRAY_H
 
+# include "scop.h"
+
+class VertexArray {
+public:
+	static std::unique_ptr<VertexArray> create();
+	~VertexArray();
+	void bind();
+	uint32_t get();
+
+private:
+	VertexArray() {};
+	bool genVao();
+
+	uint32_t m_vao{0};
+};
+
 #endif
