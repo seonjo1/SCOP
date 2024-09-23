8745be27a56c2a180cd33baf7bf310399776ea51
seonjo1 <tjdgus9684@gmail.com>
2024-09-23 11:51:33 +0900

    Update shader: shader.h, shader.cpp 구현

--- include/shader.h
+++ include/shader.h
@@ -5,9 +5,13 @@
 
 class Shader {
 public:
+	static std::unique_ptr<Shader> create(const std::string& filePath, GLenum type);
+	~Shader();
+	uint32_t get();
 
 private:
-
+	bool load(const std::string& filePath, GLenum type);
+	uint32_t m_shader{0};
 };
 
 #endif
--- src/shader.cpp
+++ src/shader.cpp
@@ -1,1 +1,49 @@
 #include "../include/shader.h"
+
+std::unique_ptr<Shader> Shader::create(const std::string& filePath, GLenum type) {
+	std::unique_ptr<Shader> shader(new Shader());
+	if (!shader->load(filePath, type)) {
+		return nullptr;
+	}
+	return shader;
+}
+
+uint32_t Shader::get() {
+	return m_shader;
+}
+
+Shader::~Shader() {
+	if (m_shader) {
+		glDeleteShader(m_shader);
+	}
+}
+
+bool Shader::load(const std::string& filePath, GLenum type) {
+	std::optional<std::string> loadShaderFileResult = glload::loadShaderFile(filePath);
+	if (!loadShaderFileResult.has_value()) {
+		return false;
+	}
+
+	std::string code = loadShaderFileResult.value();
+	int32_t codeLen = code.length();
+	const char* codePtr = code.c_str();
+
+	m_shader = glCreateShader(type);
+	glShaderSource(m_shader, 1, &codePtr, &codeLen);
+	glCompileShader(m_shader);
+
+
+	int success = 0;
+
+	glGetShaderiv(m_shader, GL_COMPILE_STATUS, &success);
+	
+	if (!success) {
+		char infoLog[1024];
+		glGetShaderInfoLog(m_shader, 1024, nullptr, infoLog);
+		std::cerr << "failed to compile shader" << std::endl;
+		std::cerr << "reason: " << infoLog << std::endl;
+		return false;
+	}
+
+	return true;
+}
