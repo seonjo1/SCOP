c57c794d1806a2aadb9725d0e085a038295577ce
seonjo1 <tjdgus9684@gmail.com>
2024-09-25 10:15:37 +0900

    Update glload.h: obj 파일 파싱을 위한 구조체와 함수 추가

--- include/glload.h
+++ include/glload.h
@@ -13,9 +13,46 @@
 
 namespace glload {
 
+struct vPos {
+	float x;
+	float y;
+	float z;
+};
+
+struct vNormal {
+	float x;
+	float y;
+	float z;
+};
+
+struct vTexCoord {
+	float x;
+	float y;
+};
+
+struct VertexInfo {
+	std::vector<vPos> vPosInfo; 
+	// std::vector<vNormal> vNormalInfo; 
+	// std::vector<vTexCoord> vTexInfo; 
+};
+
+struct iFace {
+	std::vector<uint32_t> indexVec;
+};
+
+struct IndexInfo {
+	std::vector<iFace> faces;
+};
+
+struct ObjInfo {
+	VertexInfo vertexInfo;
+	IndexInfo indexInfo;
+};
+
 bool isBmpFile(std::string fileName);
 std::unique_ptr<uint8_t[]> loadBmpImg(const char* fileName, int* width, int* height, int* channelCount);
 std::optional<std::string> loadShaderFile(const std::string& fileName);
+ObjInfo loadObjFile(const std::string& fileName);
 
 }
 
