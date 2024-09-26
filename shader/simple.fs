b0f04c19c5c2cb7a1ad24685f8085629eedf2193
seonjo1 <tjdgus9684@gmail.com>
2024-09-26 11:19:40 +0900

    Update simple.fs: color 추가 및 색 적용

--- shader/simple.fs
+++ shader/simple.fs
@@ -1,9 +1,10 @@
 #version 330 core
 in vec2 texCoord;
+in vec3 color;
 out vec4 fragColor;
 
 uniform sampler2D tex;
 
 void main() {
-	fragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+	fragColor = vec4(color, 1.0f);
 }
