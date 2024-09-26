fedc307b174c08700240a6551ccb017783290e7f
seonjo1 <tjdgus9684@gmail.com>
2024-09-26 19:48:32 +0900

    Update model: Update mesh: 오브젝트 의 움직임 관리를 model 클래스로 옮김, 그리고 m_texRatio 추가

--- include/model.h
+++ include/model.h
@@ -4,18 +4,26 @@
 # include "scop.h"
 # include "mesh.h"
 # include "program.h"
+# include <cmath>
+# include <algorithm>
 
 class Model {
 public:
 	static std::unique_ptr<Model> create(std::string fileName);
 	void draw(Program* program, glmath::vec3& cameraPos, glmath::vec3& cameraUp, glmath::vec3& cameraFront);
-	void updateModel(glmath::vec3& move, float degree);
+	void updateModel(glmath::vec3& move, float degree, float transSpeed);
 
 private:
 	bool createMeshes(const std::string& fileName);
 	void addVertex(glload::ObjInfo* objInfo, std::vector<Vertex>& vertices, std::vector<uint32_t>& indices, uint32_t idx);
+	void setColor(std::vector<Vertex>& vertices);
+	void setModelPos(std::vector<Vertex>& vertices);
 
 	std::vector<std::unique_ptr<Mesh>> m_meshes;
+	glmath::vec3 m_modelPos;
+	glmath::vec3 m_move;
+	float m_degree{0.0f};
+	float m_texRatio{0.0f};
 };
 
 #endif
--- src/model.cpp
+++ src/model.cpp
@@ -18,6 +18,36 @@ void Model::addVertex(glload::ObjInfo* objInfo, std::vector<Vertex>& vertices, s
 	indices.push_back(vertices.size() - 1);
 }
 
+void Model::setModelPos(std::vector<Vertex>& vertices) {
+	glmath::vec3 maxV(0.0f);
+	glmath::vec3 minV(0.0f);
+
+	for (int i = 0; i < vertices.size(); i++) {
+		maxV.x = std::max(maxV.x, vertices[i].pos.x);
+		maxV.y = std::max(maxV.y, vertices[i].pos.y);
+		maxV.z = std::max(maxV.z, vertices[i].pos.z);
+		minV.x = std::min(minV.x, vertices[i].pos.x);
+		minV.y = std::min(minV.y, vertices[i].pos.y);
+		minV.z = std::min(minV.z, vertices[i].pos.z);
+	}
+
+	m_modelPos = (minV + maxV) * 0.5f;
+	m_move = glmath::vec3(0.0f);
+}
+
+void Model::setColor(std::vector<Vertex>& vertices) {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	static std::uniform_real_distribution<float> dis(0.0f, 1.0f);
+	
+	for (int i = 0; i < vertices.size() / 3; i++) {
+		glmath::vec3 color(dis(gen), dis(gen), dis(gen));
+		vertices[3 * i].color = color;
+		vertices[3 * i + 1].color = color;
+		vertices[3 * i + 2].color = color;
+	}
+}
+
 bool Model::createMeshes(const std::string& fileName) {
 	std::unique_ptr<glload::ObjInfo> objInfo = glload::loadObjFile(fileName);
 	if (!objInfo) {
@@ -34,21 +64,32 @@ bool Model::createMeshes(const std::string& fileName) {
 		addVertex(objInfo.get(), vertices, indices, objInfo->indexInfo.faces[i].i3);
 	}
 	
+	setModelPos(vertices);
+	setColor(vertices);
+
 	m_meshes.push_back(Mesh::createMesh(vertices, indices, objInfo.get()));
 
 	return true;
 }
 
 void Model::draw(Program* program, glmath::vec3& cameraPos, glmath::vec3& cameraUp, glmath::vec3& cameraFront) {
+	glmath::mat4 model = glmath::translate(glmath::mat4(1.0f), m_modelPos + m_move) *
+						 glmath::rotate(glmath::mat4(1.0f), glmath::radians(m_degree), glmath::vec3(0.0f, 1.0f, 0.0f)) *
+						 glmath::scale(glmath::mat4(1.0f), glmath::vec3(1.0f)) *
+						 glmath::translate(glmath::mat4(1.0f), -1 * m_modelPos);
+						 
+	glmath::mat4 view = glmath::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+	glmath::mat4 viewModelMatrix = view * model;
+	program->setUniform("viewModel", viewModelMatrix);
+	program->setUniform("texRatio", m_texRatio);
 	for (std::unique_ptr<Mesh>& mesh : m_meshes) {		
-		glmath::mat4 viewModelMatrix = mesh->getViewModelMatrix(cameraPos, cameraUp, cameraFront);
-		program->setUniform("viewModel", viewModelMatrix);
 		mesh->draw();
 	}
 }
 
-void Model::updateModel(glmath::vec3& move, float degree) {
-	for (std::unique_ptr<Mesh>& mesh : m_meshes) {
-		mesh->updateMesh(move, degree);
-	}
+void Model::updateModel(glmath::vec3& move, float degree, float transSpeed) {
+	m_move = m_move + move;
+	m_degree = m_degree + degree;
+	if (m_degree > 360.0f) m_degree -= 360.0f;
+	m_texRatio = std::clamp(m_texRatio + transSpeed, 0.0f, 1.0f);
 }
