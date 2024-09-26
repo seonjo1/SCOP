397ab98007ead634090ba6d48b79a52655898757
seonjo1 <tjdgus9684@gmail.com>
2024-09-26 20:47:11 +0900

    Update texture.cpp : 텍스처 wrap을 repeat으로 변경

--- src/texture.cpp
+++ src/texture.cpp
@@ -13,8 +13,8 @@ void Texture::bind() {
 void Texture::setParam() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 }
 
 void Texture::genTexture(std::shared_ptr<Image> image) {
