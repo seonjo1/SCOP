5ee577236450d1a155ebc5198f885da967001e9c
seonjo1 <tjdgus9684@gmail.com>
2024-09-28 15:12:56 +0900

    Update mesh: normal 추가, color 는 따로 받아서 새로운 vbo로 전달, material 일단 주석처리

--- include/mesh.h
+++ include/mesh.h
@@ -9,42 +9,44 @@
 struct Vertex {
 	glmath::vec3 pos;
 	glmath::vec2 texCoord;
-	glmath::vec3 color;
+	glmath::vec3 normal;
 };
 
-class Material {
-public:
-	Material() = default;
-	void setMaterial(glload::Material& materialInfo);
+// class Material {
+// public:
+// 	Material() = default;
+// 	void setMaterial(glload::Material& materialInfo);
 
-private:
-	float m_Ka[3] { 0.2f, 0.2f, 0.2f };
-	float m_Kd[3] { 0.8f, 0.8f, 0.8f };
-	float m_Ks[3] { 0.5f, 0.5f, 0.5f };
-	float m_Ns { 50.0f };
-	float m_Ni { 1.0f };
-	float m_d { 1.0f };
-	uint32_t m_illum { 2 };
-};
+// private:
+// 	float m_Ka[3] { 0.2f, 0.2f, 0.2f };
+// 	float m_Kd[3] { 0.8f, 0.8f, 0.8f };
+// 	float m_Ks[3] { 0.5f, 0.5f, 0.5f };
+// 	float m_Ns { 50.0f };
+// 	float m_Ni { 1.0f };
+// 	float m_d { 1.0f };
+// 	uint32_t m_illum { 2 };
+// };
 
 class Mesh {
 public:
 	static std::unique_ptr<Mesh> createPlane();
 	static std::unique_ptr<Mesh> createMesh(std::vector<Vertex>& vertices,
-											std::vector<uint32_t>& indices, 
+											std::vector<uint32_t>& indices,
+											std::vector<glmath::vec3>& colors, 
 											glload::ObjInfo* objInfo);
 	void draw();
 
 private:
 	Mesh() = default;
-	void init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices);
-	void initMaterial(glload::Material& materialInfo);
+	void init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices, std::vector<glmath::vec3>& colors);
+	// void initMaterial(glload::Material& materialInfo);
 
 	std::unique_ptr<VertexArray> m_vertexArray;
 	std::shared_ptr<Buffer> m_vertexBuffer; 
+	std::shared_ptr<Buffer> m_vertexColorBuffer; 
 	std::shared_ptr<Buffer> m_elementBuffer;
 	GLsizei m_elementSize;
-	Material m_material;
+	// Material m_material;
 };
 
 #endif
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -9,34 +9,44 @@ std::unique_ptr<Mesh> Mesh::createPlane() {
 	std::vector<uint32_t> indices = { 0, 3, 1,
 									 1, 3, 2 };
 									 
+	std::vector<glmath::vec3> colors = {glmath::vec3(0.0f, 0.0f, 0.0f),
+										glmath::vec3(0.0f, 0.0f, 0.0f),
+										glmath::vec3(0.0f, 0.0f, 0.0f),
+										glmath::vec3(0.0f, 0.0f, 0.0f),};
+
 	std::unique_ptr<Mesh> mesh(new Mesh());
-	mesh->init(vertices, indices);
+	mesh->init(vertices, indices, colors);
 	return mesh;
 }
 
 std::unique_ptr<Mesh> Mesh::createMesh(std::vector<Vertex>& vertices,
 										std::vector<uint32_t>& indices, 
+										std::vector<glmath::vec3>& colors,
 										glload::ObjInfo* objInfo) {
 	std::unique_ptr<Mesh> mesh(new Mesh());
-	mesh->init(vertices, indices);
-	mesh->initMaterial(objInfo->marterialInfo);
+	mesh->init(vertices, indices, colors);
+	// mesh->initMaterial(objInfo->marterialInfo);
 	
 	return mesh;
 }
 
 
-void Mesh::init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices) {
-
+void Mesh::init(std::vector<Vertex>& vertices, std::vector<uint32_t>& indices, std::vector<glmath::vec3>& colors) {
 	GLsizei vertexSize = vertices.size();
+	GLsizei colorSize = colors.size();
 	m_elementSize = indices.size();
 
 	m_vertexArray = VertexArray::create();
+
 	m_vertexBuffer = Buffer::create(GL_ARRAY_BUFFER, sizeof(Vertex) * vertexSize,
 									vertices.data(), GL_STATIC_DRAW);
-
 	m_vertexArray->setAttribute(0, 3, sizeof(Vertex), offsetof(Vertex, pos));
 	m_vertexArray->setAttribute(1, 2, sizeof(Vertex), offsetof(Vertex, texCoord));
-	m_vertexArray->setAttribute(2, 3, sizeof(Vertex), offsetof(Vertex, color));
+	m_vertexArray->setAttribute(2, 3, sizeof(Vertex), offsetof(Vertex, normal));
+
+	m_vertexColorBuffer = Buffer::create(GL_ARRAY_BUFFER, sizeof(glmath::vec3) * colorSize,
+									colors.data(), GL_STATIC_DRAW);
+	m_vertexArray->setAttribute(3, 3, sizeof(glmath::vec3), 0);
 
 	m_elementBuffer = Buffer::create(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * m_elementSize,
 									indices.data(), GL_STATIC_DRAW);
@@ -47,19 +57,19 @@ void Mesh::draw() {
 	glDrawElements(GL_TRIANGLES, m_elementSize, GL_UNSIGNED_INT, 0);
 }
 
-void Material::setMaterial(glload::Material& materialInfo) {
-	for (int i = 0; i < 3; ++i) {
-		this->m_Ka[i] = materialInfo.Ka[i];
-		this->m_Kd[i] = materialInfo.Kd[i];
-		this->m_Ks[i] = materialInfo.Ks[i];
-	}
+// void Material::setMaterial(glload::Material& materialInfo) {
+// 	for (int i = 0; i < 3; ++i) {
+// 		this->m_Ka[i] = materialInfo.Ka[i];
+// 		this->m_Kd[i] = materialInfo.Kd[i];
+// 		this->m_Ks[i] = materialInfo.Ks[i];
+// 	}
 
-	this->m_Ns = materialInfo.Ns;
-	this->m_Ni = materialInfo.Ni;
-	this->m_d = materialInfo.d;
-	this->m_illum = materialInfo.illum;
-}
+// 	this->m_Ns = materialInfo.Ns;
+// 	this->m_Ni = materialInfo.Ni;
+// 	this->m_d = materialInfo.d;
+// 	this->m_illum = materialInfo.illum;
+// }
 
-void Mesh::initMaterial(glload::Material& materialInfo) {
-	this->m_material.setMaterial(materialInfo);
-}
+// void Mesh::initMaterial(glload::Material& materialInfo) {
+// 	this->m_material.setMaterial(materialInfo);
+// }
