253814739e522dbecae9f0f81cfe84cbdf81fff3
seonjo1 <tjdgus9684@gmail.com>
2024-10-01 10:35:37 +0900

    Update main.cpp: 안티 앨리어싱 추가

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,8 @@ int main(int argc, const char** argv) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);;
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	
 	std::cout << "Create glfw window" << std::endl;
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
 	if (!window) {
